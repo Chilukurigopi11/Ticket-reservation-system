e0f36a53603017d5368919409363c0ed926cdd9c
Chilukurigopi11 <100194560+Chilukurigopi11@users.noreply.github.com>
2022-04-28 14:46:10 +0530

    Update medallion.sql

--- medallion.sql
+++ medallion.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 14, 2021 at 06:49 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
